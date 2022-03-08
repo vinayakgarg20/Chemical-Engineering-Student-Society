ca029cc7b899fef9dedd802aaa2dcc4d9d4bb39d
Vinayak Garg <vinayak_g@ch.iitr.ac.in>
2022-03-08 18:56:07 +0530

    added some animation packages

--- src/components/Home/BlogSection.js
+++ src/components/Home/BlogSection.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const BlogSection = () => {
-  return <div>BlogSection</div>;
+  return <div style={{ height: "100vh" }}>BlogSection</div>;
 };
 
 export default BlogSection;
--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -6,29 +6,47 @@ const ContactUs = () => {
   };
   return (
     <div>
-      <h3>Get in touch</h3>
-      <form onSubmit={submitHandler}>
-        <div>
-          <label htmlFor="name">Name</label>
-          <input type={"text"} required id="name"></input>
-        </div>
-        <div>
-          <label htmlFor="contact-no">Contact Number</label>
-          <input type={"text"} required id="contact-no"></input>
-        </div>
-
-        <div>
-          <label htmlFor="email">Email Address</label>
-          <input type={"email"} required id="email"></input>
-        </div>
-        <div>
-          <label htmlFor="description">Type Your message here</label>
-          <textarea required rows={5} id="description"></textarea>
-        </div>
-        <div>
-          <button>Submit</button>
-        </div>
-      </form>
+      <div className="getinTouch">
+        <h3>Get in touch</h3>
+        <p></p>
+        <div></div>
+      </div>
+      <div className="Form">
+        <form onSubmit={submitHandler}>
+          <div className="Name">
+            <div>
+              <label htmlFor="Fname">First Name</label>
+              <input type={"text"} required id="Fname"></input>
+            </div>
+            <div>
+              <label htmlFor="Lname">Last Name</label>
+              <input type={"text"} required id="Lname"></input>
+            </div>
+          </div>
+          <div className="contact">
+            <div>
+              <label htmlFor="email">Email</label>
+              <input type={"email"} required id="email"></input>
+            </div>
+            <div>
+              <label htmlFor="contact-no">Phone Number</label>
+              <input type={"text"} required id="contact-no"></input>
+            </div>
+          </div>
+          <div className="message">
+            <label htmlFor="description">Message</label>
+            <textarea
+              placeholder="Write your message here..."
+              required
+              rows={5}
+              id="description"
+            ></textarea>
+          </div>
+          <div className="button">
+            <button>Submit</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
