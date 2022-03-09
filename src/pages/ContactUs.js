9b49e071846e59b01b4e09b63245749ca19d3e4a
Vinayak Garg <vinayak_g@ch.iitr.ac.in>
2022-03-09 17:14:53 +0530

    add emailjs server as database

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,94 +1,100 @@
 import React from "react";
 import "./Contact.css";
+import emailjs from "emailjs-com";
+import { useRef } from "react";
 
 const ContactUs = () => {
+  const form = useRef();
+
   const submitHandler = (event) => {
     event.preventDefault();
+    emailjs
+      .sendForm(
+        "service_3u82gxo",
+        "template_qyvr9bb",
+        form.current,
+        "fwpp4k-kljuwfDYiq"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          alert("Thank You, We'll contact you shortly!");
+        },
+        (error) => {
+          console.log(error.text);
+          alert("FAILED...", error);
+        }
+      );
   };
   return (
-    <div>
-      <div className="Container">
-        
-          <div className="getinTouch">
-          <h3>Get in touch</h3>
-          <p>The department of Chemical Engineering at IITR offers a state of the art curriculum for its students at all levels to prepare them to become leaders of tomorrow who can transform the chemical industries to solve the socio-economic problems in a sustainable manner.</p>
-          <div className="Sociallinks">
-            <p>	ch.iitr.ac.in</p>
-            <p>  +91 95872 00161</p>
-            <p>   	iitr.ac.in</p>
-            <p>   Roorkee, Uttarakhand, India</p>
-            </div>
+    <div className="Container">
+      <div className="getinTouch">
+        <h3>Get in touch.</h3>
+        <p>
+          The department of Chemical Engineering at IITR offers a state of the
+          art curriculum for its students at all levels to prepare them to
+          become leaders of tomorrow who can transform the chemical industries
+          to solve the socio-economic problems in a sustainable manner.
+        </p>
+        <div className="Sociallinks">
+          <p> ch.iitr.ac.in</p>
+          <p> +91 95872 00161</p>
+          <p> iitr.ac.in</p>
+          <p> Roorkee, Uttarakhand, India</p>
+        </div>
+      </div>
 
-          
-        
-          </div>
-        
-        <div className="Form">
-        <form onSubmit={submitHandler}>
+      <div className="Form">
+        <form ref={form} onSubmit={submitHandler}>
           <div className="Name">
             <div className="first">
-              <label htmlFor="Fname">First Name</label>
+              <label htmlFor="Fname">First Name*</label>
               <div className="search">
-                
-                  <input type={"text"} required id="Fname"></input>
-
-                
+                <input type={"text"} name="f_name" required id="Fname"></input>
               </div>
-              
-              
             </div>
             <div className="last">
               <label htmlFor="Lname">Last Name</label>
               <div className="search">
-                
-                  <input type={"text"} required id="Lname"></input>
-
-                
+                <input type={"text"} name="l_name" id="Lname"></input>
               </div>
-              
-              
             </div>
           </div>
           <div className="contact">
             <div className="emailchess">
-              <label htmlFor="email">Email</label>
+              <label htmlFor="email">Email*</label>
               <div className="search">
-                
-                  <input type={"text"} required id="email"></input>
-                
-              
+                <input type={"text"} name="email" required id="email"></input>
               </div>
             </div>
             <div className="contactchess">
               <label htmlFor="contact-no">Phone Number</label>
               <div className="search">
-                
-                  <input type={"text"} required id="contact-no"></input>
-                
-                  
-              </div>   
-              
+                <input
+                  type={"text"}
+                  name="contact_number"
+                  id="contact-no"
+                ></input>
+              </div>
             </div>
           </div>
           <div className="message">
-            <label htmlFor="description">Message</label>
-            <textarea
-              placeholder="Write your message here..."
-              required
-              rows={5}
-              id="description"
-            ></textarea>
+            <label htmlFor="description">Message*</label>
+            <div className="search">
+              <textarea
+                name="message"
+                placeholder="Write your message here..."
+                required
+                id="description"
+              ></textarea>
+            </div>
           </div>
-          <div className="button">
-            <button>Submit</button>
-          </div>
+
+          <button className="button" type="submit">
+            Submit
+          </button>
         </form>
       </div>
-        
-      </div>
-      
-      
-      
     </div>
   );
 };
